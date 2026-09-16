36579ef43d5023e2819e1ddfe506b5dfbe6739d4
Pedro Ramon Farias Lima de Jesus <pedrorfariasljesus@gmail.com>
2026-09-16 12:50:52 -0300

    Start documentation

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/**
+ * Initializes the project
+ *
+ * @author Pedro Ramon <pedrorfariasljesus@gmail.com>
+ * @version 1.0.0
+ * @since 16/09/2026
+ */
+
 public class Main {
     public static void main(String[] args) {
         String caminhoArquivo = "C:/Users/ADMIN/OneDrive/Documentos/Meus_Projetos/SistemaBancarioEvolucao/Evolution-Bank-System/contas.txt";
